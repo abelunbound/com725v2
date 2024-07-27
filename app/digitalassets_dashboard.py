bbb165669257fd2da5ec31d316300670be0028e1
Abel Akeni <5akena74@solent.ac.uk>
2024-07-27 13:39:11 +0100

    Update .py code

--- app/digitalassets_dashboard.py
+++ app/digitalassets_dashboard.py
@@ -1,21 +1,405 @@
 # app/digitalassets_dashboard.py
-from flask import Flask, request, jsonify
-from dash import Dash, html
+
+
+# Standard library imports
+import pandas as pd
+
+# Third-party imports
+import plotly.express as px
 import dash
-
-
-from dash import Dash, html
+import dash_bootstrap_components as dbc
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output, State
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H1("Hello Dash"),
-    html.Div("Welcome to Dash on Vercel!")
+
+# the style arguments for the sidebar.
+SIDEBAR_STYLE = {
+    'position': 'fixed',
+    'top': 0,
+    'left': 0,
+    'bottom': 0,
+    'width': '20%',
+    'padding': '20px 10px',
+    'background-color': '#f8f9fa'
+}
+
+# the style arguments for the main content page.
+CONTENT_STYLE = {
+    'margin-left': '25%',
+    'margin-right': '5%',
+    'padding': '20px 10p'
+}
+
+TEXT_STYLE = {
+    'textAlign': 'center',
+    'color': '#191970'
+}
+
+CARD_TEXT_STYLE = {
+    'textAlign': 'center',
+    'color': '#0074D9'
+}
+
+controls = dbc.FormGroup(
+    [
+        html.P('Select Cryptocurrency', style={
+            'textAlign': 'center'
+        }),
+        dcc.Dropdown(
+            id='dropdown',
+            options=[{
+                'label': 'Bitcoin',
+                'value': 'value1'
+            }, {
+                'label': 'Ethereum',
+                'value': 'value2'
+            },
+                {
+                    'label': 'DOGE Coin',
+                    'value': 'value3'
+                }
+            ],
+            value=['value1'],  # default value
+            multi=True
+        ),
+        html.Br(),
+        html.P('Range Slider', style={
+            'textAlign': 'center'
+        }),
+        dcc.RangeSlider(
+            id='range_slider',
+            min=0,
+            max=5,
+            step=0.5,
+            value=[5, 15]
+        ),
+        html.P('Check Box', style={
+            'textAlign': 'center'
+        }),
+        dbc.Card([dbc.Checklist(
+            id='check_list',
+            options=[{
+                'label': 'Value One',
+                'value': 'value1'
+            },
+                {
+                    'label': 'Value Two',
+                    'value': 'value2'
+                },
+                {
+                    'label': 'Value Three',
+                    'value': 'value3'
+                }
+            ],
+            value=['value1', 'value2'],
+            inline=True
+        )]),
+        html.Br(),
+        html.P('Radio Items', style={
+            'textAlign': 'center'
+        }),
+        dbc.Card([dbc.RadioItems(
+            id='radio_items',
+            options=[{
+                'label': '24 Hours',
+                'value': 'value1'
+            },
+                {
+                    'label': '7 Days',
+                    'value': 'value2'
+                },
+                {
+                    'label': '30 Days',
+                    'value': 'value3'
+                }
+            ],
+            value='value1',
+            style={
+                'margin': 'auto'
+            }
+        )]),
+        html.Br(),
+        dbc.Button(
+            id='submit_button',
+            n_clicks=0,
+            children='Submit',
+            color='primary',
+            block=True
+        ),
+    ]
+)
+
+sidebar = html.Div(
+    [
+        html.H2('Parameters', style=TEXT_STYLE),
+        html.Hr(),
+        controls
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content_first_row = dbc.Row([
+    dbc.Col(
+        dbc.Card(
+            [
+
+                dbc.CardBody(
+                    [
+                        html.H4(id='card_title_1', children=['Card Title 1'], className='card-title',
+                                style=CARD_TEXT_STYLE),
+                        html.P(id='card_text_1', children=['Sample text.'], style=CARD_TEXT_STYLE),
+                    ]
+                )
+            ]
+        ),
+        md=3
+    ),
+    dbc.Col(
+        dbc.Card(
+            [
+
+                dbc.CardBody(
+                    [
+                        html.H4('Card Title 2', className='card-title', style=CARD_TEXT_STYLE),
+                        html.P('Sample text.', style=CARD_TEXT_STYLE),
+                    ]
+                ),
+            ]
+
+        ),
+        md=3
+    ),
+
+    dbc.Col(
+        dbc.Card(
+            [
+                dbc.CardBody(
+                    [
+                        html.H4('Card Title 3', className='card-title', style=CARD_TEXT_STYLE),
+                        html.P('Sample text.', style=CARD_TEXT_STYLE),
+                    ]
+                ),
+            ]
+
+        ),
+        md=3
+    ),
+    dbc.Col(
+        dbc.Card(
+            [
+                dbc.CardBody(
+                    [
+                        html.H4('Card Title 4', className='card-title', style=CARD_TEXT_STYLE),
+                        html.P('Sample text.', style=CARD_TEXT_STYLE),
+                    ]
+                ),
+            ]
+        ),
+        md=3
+    )
 ])
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+
+content_second_row = dbc.Row(
+    [
+        dbc.Col(
+            dcc.Graph(
+                id='life-exp-vs-gdp',
+                figure=fig
+            ), md=12
+        )
+    ]
+)
+
+content_third_row = dbc.Row(
+    [
+        dbc.Col(
+            dcc.Graph(id='graph_4'), md=12,
+        )
+    ]
+)
+
+content_fourth_row = dbc.Row(
+    [
+        dbc.Col(
+            dcc.Graph(id='graph_5'), md=6
+        ),
+        dbc.Col(
+            dcc.Graph(id='graph_6'), md=6
+        )
+    ]
+)
+
+content = html.Div(
+    [
+        html.H2('SOLiGence Intelligence Trading Platform', style=TEXT_STYLE),
+        html.Hr(),
+        content_first_row,
+        content_second_row,
+        content_third_row,
+        content_fourth_row
+    ],
+    style=CONTENT_STYLE
+)
+
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = html.Div([sidebar, content])
+
+
+@app.callback(
+    Output('graph_1', 'figure'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_graph_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)
+    fig = {
+        'data': [{
+            'x': [1, 2, 3],
+            'y': [3, 4, 5]
+        }]
+    }
+    return fig
+
+
+@app.callback(
+    Output('graph_2', 'figure'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_graph_2(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)
+    fig = {
+        'data': [{
+            'x': [1, 2, 3],
+            'y': [3, 4, 5],
+            'type': 'bar'
+        }]
+    }
+    return fig
+
+
+@app.callback(
+    Output('graph_3', 'figure'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_graph_3(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)
+    df = px.data.iris()
+    fig = px.density_contour(df, x='sepal_width', y='sepal_length')
+    return fig
+
+
+@app.callback(
+    Output('graph_4', 'figure'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_graph_4(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)  # Sample data and figure
+    df = px.data.gapminder().query('year==2007')
+    fig = px.scatter_geo(df, locations='iso_alpha', color='continent',
+                         hover_name='country', size='pop', projection='natural earth')
+    fig.update_layout({
+        'height': 600
+    })
+    return fig
+
+
+@app.callback(
+    Output('graph_5', 'figure'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_graph_5(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)  # Sample data and figure
+    df = px.data.iris()
+    fig = px.scatter(df, x='sepal_width', y='sepal_length')
+    return fig
+
+
+@app.callback(
+    Output('graph_6', 'figure'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_graph_6(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)  # Sample data and figure
+    df = px.data.tips()
+    fig = px.bar(df, x='total_bill', y='day', orientation='h')
+    return fig
+
+
+@app.callback(
+    Output('card_title_1', 'children'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_card_title_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)  # Sample data and figure
+    return 'Card Tile 1'
+
+
+@app.callback(
+    Output('card_text_1', 'children'),
+    [Input('submit_button', 'n_clicks')],
+    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+     State('radio_items', 'value')
+     ])
+def update_card_text_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+    print(n_clicks)
+    print(dropdown_value)
+    print(range_slider_value)
+    print(check_list_value)
+    print(radio_items_value)  # Sample data and figure
+    return 'Card text change by call back'
+
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
-
+    app.run_server(debug=True)
 # # Create a Dash app
 # dash_app = Dash(__name__)
 #
